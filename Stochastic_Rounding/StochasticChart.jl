19471d81d60f8e93bab4827409f4e41050582530
Avleen Kaur <87992106+avleenk2312@users.noreply.github.com>
2022-09-19 17:17:55 -0600

    Update StochasticChart.jl

--- Stochastic_Rounding/StochasticChart.jl
+++ Stochastic_Rounding/StochasticChart.jl
@@ -1,4 +1,6 @@
 # This code is for "stochastich rounding" mode
+#Reference: Code in R: https://gist.github.com/coleoguy/a50dba496fc55440cb4945fe8e25d703 
+#          as mentioned in https://medium.com/@minghz42/what-is-stochastic-rounding-b78670d0c4a
 using Distributions, Plots
 
 function StochasticRound(x)
