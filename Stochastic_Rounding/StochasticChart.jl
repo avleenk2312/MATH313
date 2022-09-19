b00d5b628e113bbee4854df9a68edd7807bec55a
Avleen Kaur <87992106+avleenk2312@users.noreply.github.com>
2022-09-19 13:04:52 -0600

    Add files via upload

---
+++ Stochastic_Rounding/StochasticChart.jl
@@ -0,0 +1,34 @@
+# This code is for "stochastich rounding" mode
+using Distributions, Plots
+
+function StochasticRound(x)
+    # extract the decimal portion
+    r=trunc(x)
+    q=abs(x-r)
+    # draw a value 0 or 1 with probability
+    # based on how close we already are
+    Δx = wsample(0:1,[1-q,q],1)
+     # make it negative if x is negative
+     if(x<0)
+         Δx=-Δx
+     end
+     # return our new value
+     r+Δx[1]
+end
+
+global ℘=0
+n=10000
+print("Enter x:")
+x = readline()
+x=parse(Float64,x)
+for i=1:n
+    result=StochasticRound(x)
+    if result==ceil(x)
+        global ℘=℘+1
+    end
+end
+global ℘=℘/n
+Plots.gr() # the default GR backend look for pie charts
+x = ["ceil(x)", "floor(x)"]
+y = [℘,1-℘]
+pie(x, y, title="Stochastic Rounding", l=0.5)
